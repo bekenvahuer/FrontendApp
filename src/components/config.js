4590afee851c1db495d93e70fe907a53c0ee0851
BekenRey <bekenvahuerrey32@gmail.com>
2025-10-23 15:41:26 -0500

    commit - frontendAPP 4

--- src/components/config.js
+++ src/components/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://tama-baleless-dominick.ngrok-free.dev";
+export const API_BASE_URL = "https://tama-baleless-dominick.ngrok-free.dev/api";
 export const FRONTEND_URL = "https://frontendapp-hz83.onrender.com";
